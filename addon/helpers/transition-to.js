89b78e9590cda4104d894625bcd452d4ad58923a
martinmalinda <malindacz@gmail.com>
2017-08-19 19:56:16 +0200

    remove unneeded code

--- addon/helpers/transition-to.js
+++ addon/helpers/transition-to.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 import { restructureQP } from './url-for';
 
-
 const { inject } = Ember;
 
 export const removeRoot = (url, rootURL) => {
@@ -11,29 +10,12 @@ export const removeRoot = (url, rootURL) => {
 export default Ember.Helper.extend({
   router: inject.service(),
 
-  _makeAction(cb) {
-    // Stolen from https://github.com/DockYard/ember-route-action-helper/blob/master/addon/-private/internals.js
-    let ClosureActionModule;
-
-    if ('ember-htmlbars/keywords/closure-action' in Ember.__loader.registry) {
-      ClosureActionModule = Ember.__loader.require('ember-htmlbars/keywords/closure-action');
-    } else if ('ember-routing-htmlbars/keywords/closure-action' in Ember.__loader.registry) {
-      ClosureActionModule = Ember.__loader.require('ember-routing-htmlbars/keywords/closure-action');
-    } else {
-      ClosureActionModule = { };
-    }
-
-    cb[ClosureActionModule.ACTION] = true;
-
-    return cb;
-  },
-
   compute(params, { replace }) {
 
     const transitionFn = replace ? this.get('router').replaceWith.bind(this.get('router')) :
                                    this.get('router').transitionTo.bind(this.get('router'));
 
-    return this._makeAction(event => {
+    return event => {
 
       if(event) {
         event.preventDefault();
@@ -52,6 +34,6 @@ export default Ember.Helper.extend({
         const routeParams = restructureQP(params);
         transitionFn(...routeParams);
       }
-    });
+    };
   }
 });
